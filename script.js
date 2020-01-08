0eb60891906bf0754b72af37c697b812a93a1c56
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-01-08 14:33:31 -0800

    append button

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ $(document).ready(function() {
   var history = $("#history");
   var mainWeather = $("#main-weather");
   var days = $("#days");
-  console.log(days, mainWeather, history);
 
   $("#search-button").click(function() {
     var citySearch = $("#search").val();
-    console.log(citySearch);
+    var buttonElement = $("<button/>", {
+      text: citySearch,
+      class: "btn btn-outline-secondary btn-lg",
+      click: function() {
+        console.log(citySearch);
+      }
+    });
+    history.append(buttonElement);
   });
 });
