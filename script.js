4728cd1a9ac0381c210e990e8ac3109f6e67d6b2
Lisa Cabrera <lisaannecabrera@gmail.com>
2020-01-08 14:16:23 -0800

    onclick function works

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ $(document).ready(function() {
   var mainWeather = $("#main-weather");
   var days = $("#days");
   console.log(days, mainWeather, history);
+
+  $("#search-button").click(function() {
+    var citySearch = $("#search").val();
+    console.log(citySearch);
+  });
 });
